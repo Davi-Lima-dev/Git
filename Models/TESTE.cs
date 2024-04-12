df47fdf569b32d341bcb94feef331618d86f0e18
Davi Lima <davi.damartes@gmail.com>
2024-04-12 10:54:17 -0300

    testando aplicacao

--- Models/TESTE.cs
+++ Models/TESTE.cs
@@ -1,11 +1,11 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
 namespace Git.Models
 {
-    public class TESTE
+	public class TESTE
+	{
+	   
+	}
+	
+	 public class TESTE2
     {
        
     }
