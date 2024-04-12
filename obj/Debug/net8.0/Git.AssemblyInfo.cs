20da6736c7d8daa5ac93a6a3cd4e925e90bf615d
Davi Lima <davi.damartes@gmail.com>
2024-04-12 11:04:20 -0300

    teste

--- obj/Debug/net8.0/Git.AssemblyInfo.cs
+++ obj/Debug/net8.0/Git.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Git")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+905c887c58a3ca6a3c82bda217fbd48b8e6e21fe")]
 [assembly: System.Reflection.AssemblyProductAttribute("Git")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Git")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
